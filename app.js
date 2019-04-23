82a4cbc0555838083009394509c1d70fb76a5cac
Daniel Sangyoon Kim <ksydaniel@gmail.com>
2019-04-23 09:59:29 -0700

    solo sprint

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // TODO
+var GroceryList = (props) => (
+    <ul>
+        <GroceryListItem item={'grapes'} />
+        <GroceryListItem item={'bananna'} />
+        <GroceryListItem item={'pineapple'} />
+    </ul>
+)
+
+var GroceryListItem = (props) => (
+    <li>{props.item}</li>
+)
+
+// groceryItems = ['Oranges', 'Carrots', 'Cucumbers'];
+
+ReactDOM.render(<GroceryList />, document.getElementById('app'));
